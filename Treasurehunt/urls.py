dc8c6541a19f9e1038fd9d22d6e7478d9c53822a
Akhileshkumar-12 <prakhil143@gmail.com>
2021-10-02 12:38:01 +0530

    .

--- Treasurehunt/urls.py
+++ Treasurehunt/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +8,7 @@ urlpatterns = [
     path('',include('authentication.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
